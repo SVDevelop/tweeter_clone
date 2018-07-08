0216b76b68c1b35831a5535af9b8c2ab9cb5e4e2
Sergey <verstka.sv@mail.rugit config --global user.email verstka.sv@mailgit config --global user.email verstka.sv@mail.ru>
2018-07-08 14:17:21 +0300

    tweeter clone

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-var gulp = require('gulp');			
-var browserSync = require('browser-sync');			
-var less = require('gulp-less');			
-var notify = require('gulp-notify');			
-var plumber = require('gulp-plumber');
+var gulp = require('gulp'),		
+	browserSync = require('browser-sync'),		
+	less = require('gulp-less'),			
+	notify = require('gulp-notify'),			
+	plumber = require('gulp-plumber');
 
 gulp.task('server', ['less'], function () {
 	browserSync.init({
 		server: { baseDir: './app/'}
 	});
 });
-gulp.watch('app/**/*.html').on('change', browserSync.reload);	
-gulp.watch('app/less/*.less', ['less']);
+gulp.watch('./app/**/*.html').on('change', browserSync.reload);	
+gulp.watch('./app/less/**/*.less', ['less']);
 
 gulp.task('less', function() {
-	return gulp.src('.app/less/main.less')
+	return gulp.src('./app/less/**/*.less')
 	.pipe(plumber({
 		errorHandler: notify.onError(function(err) {
 			return {
@@ -25,7 +25,7 @@ gulp.task('less', function() {
 	}))
 	.pipe(less())
 	.pipe(gulp.dest('./app/css'))
-	.pipe(browserSync.stream())
+	.pipe(browserSync.stream());
 });
 
 gulp.task('default', ['server']);		
